1cdc30bd2f9f13fe297497fae6f9cb97990b5130
Mingsicogi <codedoctor119@gmail.com>
2020-05-13 23:02:22 +0900

    Add Circle slider implements

--- lib/widget/box_slider.dart
+++ lib/widget/box_slider.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:mins_app/model/movie.dart';
+
+class BoxSlider extends StatelessWidget {
+  final List<Movie> movies;
+  BoxSlider({this.movies});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    )
+  }
+
+}
